1750454b6acdc091fbcd75b4cda9542e8b94bb38
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2024-03-07 11:17:33 +0100

    Documentatie toegevoed aan jarfile project

--- mijnJarFileProject/src/main/java/be/kuleuven/CheckNeighboursInGrid.java
+++ mijnJarFileProject/src/main/java/be/kuleuven/CheckNeighboursInGrid.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 
 public class CheckNeighboursInGrid {
     /* rij dan  kolom*/
+
+    /**
+     *
+     * @param width  - Specifies the width of the grid.
+     * @param height - Specifies the height of the grid (extra for checking if 1D grid is complete given the specified width)
+     * @param index  - Specifies the index of the element which neighbours that need to be checked
+     * @return -Returns 1D array of parameters, 0: Row start, 1: Row end, 2: Column start, 3: Column end
+     */
     public static int[] getParameters(int width, int height, int index) {
         if (index == 0) {
             return new int[]{0, 1, 0, 1};
@@ -33,10 +41,25 @@ public class CheckNeighboursInGrid {
     }
 
     /*public static void main(String[] args) {
-        int[] a = getParameters(5, 6, 25);
-        for (int i=0; i<4; i++){
-            System.out.printf(String.valueOf(a[i]));
-        }
+        ArrayList<Integer> grid = new ArrayList<>();
+        grid.add(0);
+        grid.add(0);
+        grid.add(1);
+        grid.add(0);
+        grid.add(1);
+        grid.add(1);
+        grid.add(0);
+        grid.add(2);
+        grid.add(2);
+        grid.add(0);
+        grid.add(1);
+        grid.add(3);
+        grid.add(0);
+        grid.add(1);
+        grid.add(1);
+        grid.add(1);
+
+        getSameNeighboursIds(grid, 4, 4,5);
     }*/
 
     /**
@@ -57,23 +80,27 @@ public class CheckNeighboursInGrid {
             throw new RuntimeException("Index is grooter dan de max");
         }
 
+        // Krijg de parameters voor de dubble for lus
         int[] parameters = getParameters(width, height, indexToCheck);
 
+        // Zet Iterable om in een arraylist
         ArrayList<Integer> arrayGrid = new ArrayList<>();
         for (Integer num : grid) {
             arrayGrid.add(num);
         }
 
-        int value = arrayGrid.get(indexToCheck);
-        ArrayList<Integer> result = new ArrayList<Integer>();
+        int value = arrayGrid.get(indexToCheck); // Waarde van de techecken iddex
+        ArrayList<Integer> result = new ArrayList<>();
         for (int rij = parameters[0]; rij <= parameters[1]; rij++){
             for (int col = parameters[2]; col <= parameters[3]; col++){
                 int index = indexToCheck + rij*width + col;
 
+                // Dit is de waarden waarvan we de buren checken
                 if (index == indexToCheck){
                     continue;
                 }
 
+                // Als de waarden van buren gelijk is aan de index 
                 if (value == arrayGrid.get(index)){
                     result.add(index);
                 }
