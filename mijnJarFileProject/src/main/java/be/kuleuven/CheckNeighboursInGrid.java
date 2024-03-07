a9eb9b2d275c9caf6943506883dbff6717954c26
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2024-03-07 10:49:23 +0100

    Parameter method tegevoegd

--- mijnJarFileProject/src/main/java/be/kuleuven/CheckNeighboursInGrid.java
+++ mijnJarFileProject/src/main/java/be/kuleuven/CheckNeighboursInGrid.java
@@ -3,7 +3,41 @@ package be.kuleuven;
 import java.util.ArrayList;
 
 public class CheckNeighboursInGrid {
+    /* rij dan  kolom*/
+    public static int[] getParameters(int width, int height, int index) {
+        if (index == 0) {
+            return new int[]{0, 1, 0, 1};
+        }
+        if (index == width - 1) {
+            return new int[]{0, 1, -1, 0};
+        }
+        if (index == (height - 1) * width) {
+            return new int[]{-1, 0, 0, 1};
+        }
+        if (index == (height * width) - 1) {
+            return new int[]{-1, 0, -1, 0};
+        }
+        if (index < width) {
+            return new int[]{0, 1, -1, 1};
+        }
+        if ((index % width) == 0) {
+            return new int[]{-1, 1, 0, 1};
+        }
+        if (((index + 1) % width) == 0) {
+            return new int[]{-1, 1, -1, 0};
+        }
+        if (index > (height * width) - width) {
+            return new int[]{-1, 0, -1, 1};
+        }
+        return new int[]{-1, 1, -1, 1};
+    }
 
+    /*public static void main(String[] args) {
+        int[] a = getParameters(5, 6, 25);
+        for (int i=0; i<4; i++){
+            System.out.printf(String.valueOf(a[i]));
+        }
+    }*/
 
     /**
      * This method takes a 1D Iterable and an element in the array and gives back an iterable containing the indexes of all neighbours with the same value as the specified element
