a7dd8a37336e5da6f4c847422e781a2e6f1c6f00
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2024-04-15 16:58:18 +0200

    Added replaceCellAt

--- candycrush/src/main/java/be/kuleuven/candycrush/Board.java
+++ candycrush/src/main/java/be/kuleuven/candycrush/Board.java
@@ -5,4 +5,11 @@ public class Board<E> {
     private BoardSize boardSize;
     private CandycrushModel model;
 
+    public E getCellAt(Position position){
+        return cells[position.getIndex()];
+    }
+
+    public void replaceCellAt(Position position, E newCell){
+        cells[position.getIndex()] = newCell;
+    }
 }
